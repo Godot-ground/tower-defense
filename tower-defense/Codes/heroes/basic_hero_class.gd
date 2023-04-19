ba0f9607cd6db6be8304c24b9a7a18d36bf45c20
danial <danialabdl90@gmail.com>
2023-04-19 14:11:05 +0330

    Update basic_hero_class.gd

--- tower-defense/Codes/heroes/basic_hero_class.gd
+++ tower-defense/Codes/heroes/basic_hero_class.gd
@@ -1,1 +1,8 @@
-
+extends Hero
+class_name Basic_hero
+      
+         func _init(speed=null ,animation=null ,damage=null ,health=null): 
+                 if(speed):spd=speed 
+                 if(animation):ant=animation 
+                 if(damage):dmg=damage 
+                 if(health):hlt=health
