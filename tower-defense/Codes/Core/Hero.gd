fd38f5bbfcd8b2c16071ee6ffcf643ad461a89ee
danial <danialabdl90@gmail.com>
2023-04-20 11:57:10 +0330

    Update Hero.gd

--- tower-defense/Codes/Core/Hero.gd
+++ tower-defense/Codes/Core/Hero.gd
@@ -1,11 +1,11 @@
 class_name Hero
-         var spd=null 
-         var ant=null 
-         var dmg=null 
-         var hlt=null 
-         func getInfo(): 
-                 return { 
-                         "speed":spd,"animation":ant,"damage":dmg,"health":hlt 
-                 }         
-         func start(): 
-                 print("animation")
+var spd=null
+var ant=null
+var dmg=null
+var hlt=null
+func getInfo():
+	return {
+		"speed":spd,"animation":ant,"damage":dmg,"health":hlt
+	}	
+func start():
+	print("animation")
