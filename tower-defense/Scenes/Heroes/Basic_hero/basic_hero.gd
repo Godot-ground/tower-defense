619b57018d0da4ae3d5f47fb5b7b307e11595bb3
danial <danialabdl90@gmail.com>
2023-04-20 13:10:55 +0330

    Update and rename a to basic_hero.gd

---
+++ tower-defense/Scenes/Heroes/Basic_hero/basic_hero.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var cls 
+@export var speed = 300
+@export var animation = Animation
+@export var damage=1
+@export var health=100
+func _ready():
+	cls =  Basic_hero.new(speed,animation,damage,health)
+	print(cls.getInfo())
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
