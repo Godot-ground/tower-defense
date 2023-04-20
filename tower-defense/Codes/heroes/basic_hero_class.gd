a4fb2e8f1230a2b0d78ac674d88681de883916aa
danial <danialabdl90@gmail.com>
2023-04-20 13:20:15 +0330

    Delete basic_hero_class.gd

--- tower-defense/Codes/heroes/basic_hero_class.gd
+++
@@ -1,6 +0,0 @@
-extends Hero
-class_name Basic_hero  
-func _init(speed=null ,animation=null ,damage=null ,health=null): 
-	if(speed):spd=speed 
-	if(animation):ant=animation 
-	if(health):hlt=health
