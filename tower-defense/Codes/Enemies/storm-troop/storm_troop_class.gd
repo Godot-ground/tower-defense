616b43d8edc4399c25705adac118a94ac2d14b5f
danial <danialabdl90@gmail.com>
2023-04-20 12:03:45 +0330

    Update storm_troop_class.gd

--- tower-defense/Codes/Enemies/storm-troop/storm_troop_class.gd
+++ tower-defense/Codes/Enemies/storm-troop/storm_troop_class.gd
@@ -1,5 +1,5 @@
 extends Enemy
-class_name Storm_troop_class
+class_name Storm_troop
 func _init(speed=null ,animation=null ,damage=null ,health=null):
 		if(speed):spd=speed
 		if(animation):ant=animation
